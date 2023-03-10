dfdb1764aa30c52061263613a5d7558a08f496f3
shmn7iii <46078198+shmn7iii@users.noreply.github.com>
2023-03-10 22:07:23 +0900

    update: separate getChatCompletion function

--- internal/hey/event_handler.go
+++ internal/hey/event_handler.go
@@ -12,6 +12,7 @@ import (
 
 var (
 	openAiGptClient *openai.Client = openai.NewClient(os.Getenv("OPENAI_TOKEN"))
+	botMentionString string = "<@" + os.Getenv("BOT_USER_ID") + ">"
 )
 
 func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -24,13 +25,13 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	if !strings.HasPrefix(m.Content, "<@842360360006582283>") {
+	if !strings.HasPrefix(m.Content, botMentionString) {
 		return
 	}
 
 	s.ChannelTyping(mentionedChannelID)
 
-	mentionedMessageContent := strings.Replace(m.Content, "<@842360360006582283>", "", -1)
+	mentionedMessageContent := strings.Replace(m.Content, botMentionString, "", -1)
 
 	if len([]rune(mentionedMessageContent)) > 50 {
 		s.ChannelMessageSendReply(
@@ -44,7 +45,23 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	commandResponseContent := "⚠️ 500 Internal Server Error"
+	chatCompletionOutputMessage, err := getChatCompletion(mentionedMessageContent, authorUsername)
+	if err != nil {
+		log.Fatalf("Error while gpt3.5-turbo request: %v", err)
+	}
+
+	s.ChannelMessageSendReply(
+		m.ChannelID,
+		chatCompletionOutputMessage,
+		&discordgo.MessageReference {
+			MessageID: mentionedMessage.ID,
+			ChannelID: mentionedChannelID,
+			GuildID: mentionedGuildID,
+	})
+}
+
+func getChatCompletion(inputMessage string, username string) (outputMessage string, err error) {
+	outputMessage = "⚠️ 500 Internal Server Error"
 
 	chatRequestSystemMessage := `ゲーム「モンスターハンター」のモンスターである「ギィギ」との会話をシミュレートします。
 															彼は子供です。彼の発言サンプルを以下に列挙します。
@@ -54,7 +71,7 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 															上記を参考に口調のみを模倣し、「ギィギ」になりきり回答を構築してください。
 															自己紹介の必要はありません。`
-	chatRequestSystemMessage	+= "また、会話相手の名前は" + authorUsername + "です。"
+	chatRequestSystemMessage	+= "また、会話相手の名前は" + username + "です。"
 
 	chatGPTResp, err := openAiGptClient.CreateChatCompletion(
 		context.Background(),
@@ -67,26 +84,19 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 				},
 				{
 					Role:    openai.ChatMessageRoleUser,
-					Content: mentionedMessageContent,
+					Content: inputMessage,
 				},
 			},
 		},
 	)
 	if err != nil {
-		log.Fatalf("Error while gpt3.5-turbo request: %v", err)
-	} else {
-		commandResponseContent = chatGPTResp.Choices[0].Message.Content
-		log.Println("[Hey gigi]")
-		log.Println("InputContent: " + mentionedMessageContent)
-		log.Println("CommandResponseContent: " + commandResponseContent)
+		return
 	}
 
-	s.ChannelMessageSendReply(
-		m.ChannelID,
-		commandResponseContent,
-		&discordgo.MessageReference {
-			MessageID: mentionedMessage.ID,
-			ChannelID: mentionedChannelID,
-			GuildID: mentionedGuildID,
-	})
+	outputMessage = chatGPTResp.Choices[0].Message.Content
+	log.Println("[Hey gigi]")
+	log.Println("InputContent: " + inputMessage)
+	log.Println("CommandResponseContent: " + outputMessage)
+
+	return
 }
